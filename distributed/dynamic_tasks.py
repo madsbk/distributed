ac6ac97abc15c9177bd7588f6624a62e8f264ff9
Mads R. B. Kristensen <madsbk@gmail.com>
2020-06-04 17:00:02 +0200

    tmp

--- distributed/dynamic_tasks.py
+++ distributed/dynamic_tasks.py
@@ -76,11 +76,12 @@ def dynshuffle_kernel(df, parts, rearguard, col, ignore_index):
     worker = get_worker()
     client = get_client()
     myself = worker.get_current_task()
+    myid = parts.index(myself)
     assert myself in parts
 
-    print(
-        f"[{worker.address}] _apply_func() - key: {repr(myself)}, df: {type(df)}, parts: {parts}, rearguard: {rearguard}, col: {repr(col)}"
-    )
+    # print(
+    #     f"[{worker.address}] _apply_func() - key: {repr(myself)}, myid: {myid}, df: {type(df)}, parts: {parts}, rearguard: {rearguard}, col: {repr(col)}"
+    # )
 
     groups = shuffle_group(
         df, col, 0, len(parts), len(parts), ignore_index=ignore_index, nfinal=len(parts)
@@ -88,12 +89,12 @@ def dynshuffle_kernel(df, parts, rearguard, col, ignore_index):
     assert len(groups) == len(parts)
 
     new_tasks = []
-    for i, part in enumerate(parts):
+    for part in parts:
         new_tasks.append(
             {
                 "key": f"shuffle_getitem_{myself}_{part}",
                 "deps": [part],
-                "task": dumps_task((getitem, part, i)),
+                "task": dumps_task((getitem, part, myid)),
                 "priority": 0,
             }
         )
@@ -121,7 +122,7 @@ def dynshuffle_kernel(df, parts, rearguard, col, ignore_index):
 def rearrange_by_column_dynamic_tasks(
     df, column, max_branch=32, npartitions=None, ignore_index=False
 ):
-    print(f"rearrange_by_column_dynamic_tasks() - column: {column}, \nddf: {df.compute()}")
+    #print(f"rearrange_by_column_dynamic_tasks() - column: {column}, \nddf: {df.compute()}")
     return dd_dynamic_tasks_map(
         dynshuffle_kernel, df, df._meta, "dynshuffle", col=column, ignore_index=ignore_index
     )
@@ -130,83 +131,83 @@ def rearrange_by_column_dynamic_tasks(
 
 
 
-def kernel(df, peers, rearguard, col):
-    worker = get_worker()
-    client = get_client()
-    peers = [p[1:] for p in peers]
-    rearguard = rearguard[1:]
-    myself = get_worker().get_current_task()
+# def kernel(df, peers, rearguard, col):
+#     worker = get_worker()
+#     client = get_client()
+#     peers = [p[1:] for p in peers]
+#     rearguard = rearguard[1:]
+#     myself = get_worker().get_current_task()
 
-    print(
-        f"[{worker.address}] kernel() - key: {myself}, peers: {peers}, rearguard: {rearguard}"
-    )
+#     print(
+#         f"[{worker.address}] kernel() - key: {myself}, peers: {peers}, rearguard: {rearguard}"
+#     )
 
-    groups = shuffle_group(df, col, 0, len(peers), len(peers), ignore_index=False, nfinal=len(peers))
-    assert len(groups) == len(peers)
+#     groups = shuffle_group(df, col, 0, len(peers), len(peers), ignore_index=False, nfinal=len(peers))
+#     assert len(groups) == len(peers)
 
-    new_tasks = []
-    for i, peer in enumerate(peers):
-        new_tasks.append(
-            {
-                "key": f"shuffle_getitem_{myself}_{peer}",
-                "deps": [peer],
-                "task": dumps_task((getitem, peer, i)),
-                "priority": 0,
-            }
-        )
-    getitem_keys = [t["key"] for t in new_tasks]
+#     new_tasks = []
+#     for i, peer in enumerate(peers):
+#         new_tasks.append(
+#             {
+#                 "key": f"shuffle_getitem_{myself}_{peer}",
+#                 "deps": [peer],
+#                 "task": dumps_task((getitem, peer, i)),
+#                 "priority": 0,
+#             }
+#         )
+#     getitem_keys = [t["key"] for t in new_tasks]
 
-    new_tasks.append(
-        {
-            "key": f"shuffle_join_{myself}",
-            "deps": [myself] + getitem_keys,
-            "task": dumps_task((_concat, getitem_keys)),
-        }
-    )
+#     new_tasks.append(
+#         {
+#             "key": f"shuffle_join_{myself}",
+#             "deps": [myself] + getitem_keys,
+#             "task": dumps_task((_concat, getitem_keys)),
+#         }
+#     )
 
-    client.sync(
-        worker.scheduler.extend_current_task,
-        cur_key=myself,
-        new_tasks=new_tasks,
-        rearguard_key=rearguard,
-        rearguard_input=f"shuffle_join_{myself}",
-    )
+#     client.sync(
+#         worker.scheduler.extend_current_task,
+#         cur_key=myself,
+#         new_tasks=new_tasks,
+#         rearguard_key=rearguard,
+#         rearguard_input=f"shuffle_join_{myself}",
+#     )
 
-    return groups
-
-
-def noop(df):
-    return df
+#     return groups
 
 
+# def noop(df):
+#     return df
 
 
-def rearrange_by_column_dynamic_tasks(
-    df, column, max_branch=32, npartitions=None, ignore_index=False
-):
-    print(f"rearrange_by_column_dynamic_tasks() - column: {column}, \nddf: {df.compute()}")
 
-    token = tokenize(df, column, max_branch, npartitions, ignore_index)
-    delayed_df = df.to_delayed()
-    delayed_kernel = dask.delayed(kernel)
-    rearguard = dask.delayed(noop)
-    kernel_names = [f"shuffle_{i}_{token}" for i in range(len(delayed_df))]
-    kernel_names_encoded = ["_shuffle_%d" % i for i in range(len(delayed_df))]
-    rearguard_names_encoded = ["%s_rearguard" % k for k in kernel_names_encoded]
 
-    res = [
-        rearguard(
-            delayed_kernel(
-                d,
-                kernel_names_encoded,
-                f"{kernel_names_encoded[i]}_rearguard",
-                column,
-                dask_key_name=kernel_names[i],
-            ),
-            dask_key_name=f"{kernel_names[i]}_rearguard",
-        )
-        for i, d in enumerate(delayed_df)
-    ]
-    print(res)
+# def rearrange_by_column_dynamic_tasks(
+#     df, column, max_branch=32, npartitions=None, ignore_index=False
+# ):
+#     print(f"rearrange_by_column_dynamic_tasks() - column: {column}, \nddf: {df.compute()}")
 
-    return dd.from_delayed(res, meta=df._meta)
+#     token = tokenize(df, column, max_branch, npartitions, ignore_index)[:6]
+#     delayed_df = df.to_delayed()
+#     delayed_kernel = dask.delayed(kernel)
+#     rearguard = dask.delayed(noop)
+#     kernel_names = [f"shuffle_{i}_{token}" for i in range(len(delayed_df))]
+#     kernel_names_encoded = [f"_shuffle_{i}_{token}" for i in range(len(delayed_df))]
+#     rearguard_names_encoded = ["%s_rearguard" % k for k in kernel_names_encoded]
+
+#     res = [
+#         rearguard(
+#             delayed_kernel(
+#                 d,
+#                 kernel_names_encoded,
+#                 f"{kernel_names_encoded[i]}_rearguard",
+#                 column,
+#                 dask_key_name=kernel_names[i],
+#             ),
+#             dask_key_name=f"{kernel_names[i]}_rearguard",
+#         )
+#         for i, d in enumerate(delayed_df)
+#     ]
+#     print(res)
+
+#     return dd.from_delayed(res, meta=df._meta)
